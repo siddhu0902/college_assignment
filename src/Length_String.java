257245b1853f5e6486a56a6b12be9cd9c0c93349
Siddharth <siddhu0902.c@gmail.com>
2026-02-07 10:27:39 +0530

    The length of the String is

---
+++ src/Length_String.java
@@ -0,0 +1,7 @@
+public class Length_String {
+    public static void main(String[] args){
+        String s1="welcome";
+        int b =s1.length();
+        System.out.println("The lenght of the String is: "+b);
+    }
+}
